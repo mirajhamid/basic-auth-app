25a8d50bd7000501b2c6f678e6a606b510e74379
Miraj Hamid <Miraj_105288@105288-005L.dialog.dialoggsm.com>
2020-12-29 13:27:55 +0530

    update for yml and APIs

--- src/main/java/com/miraj/basicauthapp/BasicAuthController.java
+++ src/main/java/com/miraj/basicauthapp/BasicAuthController.java
@@ -1,19 +1,23 @@
 package com.miraj.basicauthapp;
 
 import java.util.ArrayList;
+
 import java.util.List;
 import java.util.Random;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
+
 @RestController
+@RequestMapping(value = "/api", produces = {APPLICATION_JSON_VALUE})
 public class BasicAuthController {
-	
-	@RequestMapping(value = "/test", method = RequestMethod.GET)
+
+	@GetMapping(value = "/test",consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
 	public ResponseEntity<OurResponse> getUser() {
 		
 		Random random = new Random();
